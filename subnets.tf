9a8ca1c2aae00dff7b31f8b8357b8f12ee5c29de
Vansl1220 <wi22b110@technikum-wien.at>
2025-12-08 15:43:29 +0100

    Added web, ai, and vm subnets

---
+++ subnets.tf
@@ -0,0 +1,32 @@
+//Required for App Service VNet integration + secure networking, per assignment
+resource "azurerm_subnet" "web" {
+  name                 = "web-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  delegation {
+    name = "webapp-delegation"
+
+    service_delegation { //must-have per assignment
+      name    = "Microsoft.Web/serverFarms"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/action"
+      ]
+    }
+  }
+}
+// hosts private endpoint, for privacy and security
+resource "azurerm_subnet" "ai" {
+  name                 = "ai-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+// hosts VM1+VM2
+resource "azurerm_subnet" "vm" {
+  name                 = "vm-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
