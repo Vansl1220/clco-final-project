c02b9e83c7a26d3db2aec2e94e63b208632e3688
Vansl1220 <wi22b110@technikum-wien.at>
2025-12-08 15:33:30 +0100

    Initial clean Terraform project

---
+++ variables.tf
@@ -0,0 +1,23 @@
+# variables.tf
+
+variable "resource_group_name" {
+  description = "Name for the Azure Resource Group"
+  type        = string
+  default     = "terraform-project"
+}
+
+variable "location" {
+  description = "Azure region norwayeast"
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "Name for the Azure storrage account"
+  type        = string
+}
+
+variable "replication_type" {
+  description = "Replication type for the storage account ( ZRS )."
+  type        = string
+  default     = "ZRS"
+}
