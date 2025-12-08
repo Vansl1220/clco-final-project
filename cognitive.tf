e7e4c360732a3554ff54283d059fcb65dc1b4929
Vansl1220 <wi22b110@technikum-wien.at>
2025-12-08 16:18:33 +0100

    Fix Cognitive Service SKU for Azure Student subscription

--- cognitive.tf
+++ cognitive.tf
@@ -3,12 +3,12 @@ resource "azurerm_cognitive_account" "lang" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
-  kind     = "TextAnalytics"
+  kind     = "CognitiveServices"
   sku_name = "S0"
 
   #forces Azure to generate a private-only endpoint
   public_network_access_enabled = false
-  
+
   tags = {
     project = "clco"
   }
